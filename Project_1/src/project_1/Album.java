080e949976008744f16e7ad32dca9556d19611e5
Ethan Damien <ethanchang.d@gmail.com>
2021-09-11 11:48:19 -0400

    sexyman

--- Project_1/src/project_1/Album.java
+++ Project_1/src/project_1/Album.java
@@ -1,4 +1,19 @@
 package project_1;
 
 public class Album {
+    private String title;
+    private String artist;
+    private Genre genre; //enum class; Classical, Country, Jazz, Pop, Unknown
+    private Date releaseDate;
+    private boolean isAvailable;
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "test String";
+    }
 }
--- Project_1/src/project_1/Date.java
+++ Project_1/src/project_1/Date.java
@@ -1,4 +1,31 @@
 package project_1;
 
-public class Date {
+public class Date implements Comparable<Date> {
+    private int year;
+    private int month;
+    private int day;
+    private final int JANUARY = 1;
+    private final int FEBRUARY = 2;
+    private final int MARCH = 3;
+    private final int APRIL = 4;
+    private final int MAY = 5;
+    private final int JUNE = 6;
+    private final int JULY = 7;
+    private final int AUGUST = 8;
+    private final int SEPTEMBER = 9;
+    private final int OCTOBER = 10;
+    private final int NOVEMBER = 11;
+    private final int DECEMBER = 12;
+    
+    public Date(String date) {} //take “mm/dd/yyyy” and create a Date object
+    public Date() {} //create an object with today’s date (see Calendar class)
+
+    public boolean isValid() {
+        return false;
+    }
+
+    @Override
+    public int compareTo(Date date) {
+        return 1;
+    }
 }
