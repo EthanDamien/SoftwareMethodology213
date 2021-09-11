7c46d33a266308beafcbd7d5e2d40fd58919c646
Ethan Damien <ethanchang.d@gmail.com>
2021-09-11 18:07:08 -0400

    Started framework for Collection.java

--- Project_1/src/project_1/Collection.java
+++ Project_1/src/project_1/Collection.java
@@ -4,19 +4,32 @@ public class Collection {
     private Album[] albums;
     private int numAlbums; //number of albums currently in the collection
 
+    public Collection(){
+        albums = new Album[4];
+    }
 
     private int find(Album album) {
         return 0;
     } //find the album index, or return NOT_FOUND
 
     private void grow() {
+        int currentLength = albums.length;
+        //Create new Albums array with 4 more entries
+        Album[] newCollection = new Album[albums.length+4];
+        //Copy old Album Array contents to new
+        System.arraycopy(albums, 0, newCollection, 0, numAlbums);
+        albums = newCollection;
     } //increase the capacity of the array list by 4
 
     public boolean add(Album album) {
+
+        numAlbums++;
         return false;
     }
 
     public boolean remove(Album album) {
+
+        numAlbums--;
         return false;
     }
 
@@ -29,12 +42,23 @@ public class Collection {
     } //set to available
 
     public void print() {
-
+        for(Album entry: albums){
+            //print list in specified format
+        }
     } //display the list without specifying the order
 
     public void printByReleaseDate() {
+        //copy the Album to a new sorted Album list
+        Album[] sortedAlbum = new Album[numAlbums];
+        System.arraycopy(albums, 0, sortedAlbum, 0, numAlbums);
+        //This is where we sort by Release Date
+
     }
 
     public void printByGenre() {
+        Album[] sortedAlbum = new Album[numAlbums];
+        System.arraycopy(albums, 0, sortedAlbum, 0, numAlbums);
+        //This is where we sort by Genre
+
     }
 }
