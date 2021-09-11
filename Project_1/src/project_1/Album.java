8d288456d32dbdfef9be4555a05dca80e4248372
Kevin Cubillos <kevincubillos.KC@gmail.com>
2021-09-11 18:23:50 -0400

    Attempted coding Genre Enum

--- Project_1/src/project_1/Album.java
+++ Project_1/src/project_1/Album.java
@@ -7,9 +7,25 @@ public class Album {
     private Date releaseDate;
     private boolean isAvailable;
 
+    public Album(String title, String artist, String genre, String date){
+        this.releaseDate = new Date(date);
+        this.genre = new Genre(genre);
+        this.title = title;
+        this.artist = artist;
+        this.isAvailable = true;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
     @Override
     public boolean equals(Object obj) {
-        return false;
+        return true;
     }
 
     @Override
--- Project_1/src/project_1/Genre.java
+++ Project_1/src/project_1/Genre.java
@@ -1,4 +1,36 @@
 package project_1;
 
+import java.util.Locale;
+
 public class Genre {
+
+    public enum GenreName{
+        CLASSICAL,
+        COUNTRY,
+        JAZZ,
+        POP,
+        UNKNOWN
+    }
+
+    private GenreName genre;
+
+    public Genre(String genre){
+        genre = genre.toUpperCase(Locale.ROOT);
+        if(genre == "CLASSICAL"){
+            this.genre = GenreName.CLASSICAL;
+        }
+        else if(genre == "COUNTRY"){
+            this.genre = GenreName.COUNTRY;
+        }
+        else if(genre == "JAZZ"){
+            this.genre = GenreName.JAZZ;
+        }
+        else if(genre == "POP"){
+            this.genre = GenreName.POP;
+        }
+        else{
+            this.genre = GenreName.UNKNOWN;
+        }
+    }
+
 }
