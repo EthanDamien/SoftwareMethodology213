7ab1dd2157468b4b37a41303ba1756a9f77e06b9
kwcubillos <60185845+kwcubillos@users.noreply.github.com>
2021-09-27 15:56:26 -0400

    Latest JavaDoc

---
+++ Project_1/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"project_1","l":"Album"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"project_1","l":"Collection"},{"p":"project_1","l":"CollectionManager"},{"p":"project_1","l":"Date"},{"p":"project_1","l":"DateTest"},{"p":"project_1","l":"Genre"},{"p":"project_1","l":"RunProject1"}];updateSearchResults();
