07be4700bb9cd5524d78150d6d08e46ef6c1aa76
Kevin Cubillos <kevincubillos.KC@gmail.com>
2021-10-10 17:27:24 -0400

    Roster JavaDoc

--- Project_2/src/project_2/Roster.java
+++ Project_2/src/project_2/Roster.java
@@ -1,23 +1,42 @@
 package project_2;
 
+/**
+ * A class that represents a Roster of Students
+ * @author Kevin Cubillos, Ethan Chang
+ */
 public class Roster {
-
+    /** The list of students for the roster */
     private Student[] roster;
+    /** The number of students in the array (neglects null elements)*/
     private int size;
+    /** represents a student could not be found */
     private static final int NOT_FOUND = -1;
+    /** represents when two students have the same name */
     private static final int SAME_NAME = 0;
+    /** represents when the roster is empty */
     public static final int EMPTY = 0;
 
+    /**
+     * Basic constructor that sets the capacity of roster to 4
+     */
     public Roster(){
-        //Hello
         roster = new Student[4];
         size = 0;
     }
 
+    /**
+     * Getter for current size of roster
+     * @return
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Find a student based on name and major
+     * @param student the target student
+     * @return the index of target student
+     */
     private int find(Student student){
         for(int i = 0; i < size; i++){
             if(roster[i].getProfile().equals(student.getProfile())){
@@ -27,17 +46,24 @@ public class Roster {
         return NOT_FOUND;
     }
 
-    public Student makePayment(Student student){
-        for(int i = 0; i < size; i++){
-            if(roster[i].getProfile().equals(student.getProfile())){
-                return roster[i];
-            }
+    /**
+     * Finds a student and returns it
+     * This method returns
+     * @param student the target student
+     * @return the target student, null if not found
+     */
+    public Student getAStudent(Student student){
+        int index = find(student);
+        if(index != NOT_FOUND){
+            return roster[index];
         }
         System.out.println("Student Not Found");
         return null;
     }
 
-
+    /**
+     * Updates capacity of roster by increasing it by 4
+     */
     private void grow(){
         Student[] temp = new Student[size+4];
         for(int i = 0; i < size; i++){
@@ -47,6 +73,11 @@ public class Roster {
         roster = temp;
     }
 
+    /**
+     * Adds a student to the roster
+     * @param student the new student being added
+     * @return true if student was added, false if student already exists
+     */
     public boolean add(Student student){
         if(find(student) != NOT_FOUND){
             return false;
@@ -59,6 +90,12 @@ public class Roster {
         return true;
     }
 
+    /**
+     * Removes a student from the roster
+     * Keeps the order of students the same after the deletion happens
+     * @param student the target student
+     * @return true if student was deleted, false if student does not exist
+     */
     public boolean remove(Student student){
         int index = find(student);
         if(index == NOT_FOUND){
@@ -72,18 +109,22 @@ public class Roster {
         return true;
     }
 
-    public boolean calculate() {
-
-        if(size == EMPTY){
-            return false;
-        }
+    /**
+     * Calculates the tuition that is due for each student
+     */
+    public void calculate() {
 
         for(int i = 0; i < size; i++){
             roster[i].tuitionDue();
         }
-        return true;
+
     }
 
+    /**
+     * Updates the studying abroad status of an international student
+     * @param student the target student
+     * @return true if update was executed, false if student does not exist
+     */
     public boolean setAbroadStatus(Student student){
 
         int index = find(student);
@@ -96,7 +137,9 @@ public class Roster {
         return true;
     }
 
-
+    /**
+     * Prints all students in roster
+     */
     public void print(){
 
         for(int i = 0; i < size; i++){
@@ -104,6 +147,9 @@ public class Roster {
         }
     }
 
+    /**
+     * Prints all students by name in alphabetical order
+     */
     public void printByName(){
 
         mergeSort(roster, 0, size-1, 2);
@@ -114,6 +160,10 @@ public class Roster {
 
     }
 
+    /**
+     * Prints all students in order from oldest to youngest latest payment date
+     * Includes only students who made at least one payment
+     */
     public void printByPaymentDate(){
         int count = 0;
         for(int i = 0; i < size; i++) {
@@ -138,7 +188,13 @@ public class Roster {
 
     }
 
-
+    /**
+     * Merge Sort algorithm to sort collections based on order.
+     * @param temp roster to be sorted
+     * @param l left index of current sub-array
+     * @param r right index of current sub-array
+     * @param order type of order for sort: 1 for payment date, 2 for name
+     */
     private void mergeSort(Student[] temp, int l, int r, int order){
         if(l >= r){
             return;
@@ -158,6 +214,13 @@ public class Roster {
         }
     }
 
+    /**
+     * Merge Algorithm of MergeSort where genres are compared.
+     * @param temp roster to be sorted
+     * @param l left index of current sub-array
+     * @param m middle index of current sub-array
+     * @param r right index of current sub-array
+     */
     private void mergeName(Student[] temp, int l, int m, int r){
         int sizeL = m - l + 1;
         int sizeR = r - m;
@@ -198,6 +261,13 @@ public class Roster {
         }
     }
 
+    /**
+     * Merge Algorithm of MergeSort where release dates are compared.
+     * @param temp a temp sub-array of roster to be sorted
+     * @param l left index of current sub-array
+     * @param m middle index of current sub-array
+     * @param r right index of current sub-array
+     */
     private void mergeDate(Student[] temp, int l, int m, int r){
         int sizeL = m - l + 1;
         int sizeR = r - m;
