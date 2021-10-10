bb8b2b4acef3847c55260f45ef81f4dc4325fcd5
Kevin Cubillos <kevincubillos.KC@gmail.com>
2021-10-10 19:25:28 -0400

    International Javadoc

--- Project_2/src/project_2/International.java
+++ Project_2/src/project_2/International.java
@@ -1,17 +1,36 @@
 package project_2;
 
+/**
+ * The subclass that represents an international student
+ * A subclass of NonResident
+ * @author Kevin Cubillos, Ethan Chang
+ */
 public class International extends NonResident{
 
+    /** tracks if international student is studying abroad */
     private boolean studyingAbroad;
 
+    /** additional fee for international students */
     private static final int ADD_FEE = 2650;
+    /** minimum amount of credits an international student can take */
     private static final int MIN_CREDITS = 12;
 
+    /**
+     * Constructor of an International where name, major, credits, and abroad status is known
+     * @param name the name of student
+     * @param major the major of student
+     * @param credits the amount of credits
+     * @param abroad studying abroad status
+     */
     public International(String name, String major, int credits, boolean abroad){
         super(name, major, credits);
         studyingAbroad = abroad;
     }
 
+    /**
+     * Updates international student that is now studying abroad
+     * Sets tuition to 0 and credits to 12 and then recalculates tuition
+     */
     public void setStudyingAbroad() {
         if(getCredits() > MIN_CREDITS){
             setCredits(MIN_CREDITS);
@@ -23,12 +42,19 @@ public class International extends NonResident{
         tuitionDue();
     }
 
+    /**
+     * Computes tuition due for an international student
+     */
     @Override
     public void tuitionDue(){
         int tuition = studyingAbroad ? 0 : FULL_TUIT;
         setTuition(tuition + UNI_FEE + ADD_FEE);
     }
 
+    /**
+     * Makes a string representation of an International
+     * @return the string representation
+     */
     @Override
     public String toString(){
         String date = getLastPaymentDate() == null ? "--/--/--" : getLastPaymentDate().toString();
