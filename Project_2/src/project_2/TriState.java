a8085da9e1528d766b9a76e6512cc281ea576109
Kevin Cubillos <kevincubillos.KC@gmail.com>
2021-10-10 19:14:47 -0400

    TriState Javadoc

--- Project_2/src/project_2/TriState.java
+++ Project_2/src/project_2/TriState.java
@@ -1,13 +1,29 @@
 package project_2;
 
+/**
+ * The subclass that represents a tri-state student
+ * A subclass of NonResident
+ * @author Kevin Cubillos, Ethan Chang
+ */
 public class TriState extends NonResident{
+    /** The tri-state the student is from */
     private String state;
 
+    /**
+     * Constructor of a TriState where name, major, credits, and state is known
+     * @param name the name of student
+     * @param major the major of student
+     * @param credits the amount of credits
+     * @param state the tri-state student is from
+     */
     public TriState(String name, String major, int credits, String state){
         super(name, major, credits);
         this.state = state;
     }
 
+    /**
+     * Computes tuition due for a tri-state student
+     */
     @Override
     public void tuitionDue(){
         int credits = getCredits();
@@ -32,6 +48,10 @@ public class TriState extends NonResident{
         setTuition(whichTime + excess + fee - triDiscount);
     }
 
+    /**
+     * Makes a string representation of a TriState
+     * @return the string representation
+     */
     @Override
     public String toString(){
         String date = getLastPaymentDate() == null ? "--/--/--" : getLastPaymentDate().toString();
