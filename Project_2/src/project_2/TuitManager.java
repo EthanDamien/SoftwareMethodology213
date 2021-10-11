63a71672b1fe0ba59179c559d80b6e3c6f42c26d
Kevin Cubillos <kevincubillos.KC@gmail.com>
2021-10-11 09:15:32 -0400

    Tokenizer JavaDoc

--- Project_2/src/project_2/TuitManager.java
+++ Project_2/src/project_2/TuitManager.java
@@ -4,14 +4,51 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+/**
+ * This is the main running class that handles valid inputs and operations based on the requirements given in Project 2.
+ * It takes in inputs with the format for adding -> (Instruction, Name, Major, CreditHours, ExtraField (applying to AT and AI).
+ *      Add Instruction:
+ *          AR -> Add Resident Student
+ *          AN -> Add non-Resident Student
+ *          AT -> Add tristate student, (extra field allows NY and CT)
+ *          AI -> Add international student, (extra field is a boolean, true if they study abroad, false if not)
+ *          R -> Remove a resident student from the roster
+ * It takes in action instructions.
+ *      Action Instructions:
+ *          R, name, major -> Removes resident student from the roster
+ *          C -> Calculate tuition for all students from the roster
+ *          T, Name, Major, Payment, Date -> Pay tuition
+ *          S, Name, Major, StudyAbroadStatus -> Set Study abroad status
+ *          F, Name, Major, FinancialAid -> Set financial aid amount
+ *          P -> print roster as is
+ *          PN -> print roster sorted by student names
+ *          PT -> print only the students who have made payments, ordered by payment dates.
+ *
+ * @author Kevin Cubillos, Ethan Chang
+ */
 public class TuitManager {
 
+    /** Checks if payment is negative */
     private static final double INVALID_PAY = 0;
+    /** Checks if credits is negative */
     private static final int INVALID_CREDIT = 0;
+    /** Index of the name in profile created through command line */
     private static final int NAME_INDEX = 0;
+    /** Index of the major in profile created through command line */
     private static final int MAJOR_INDEX = 0;
+    /** Printing type for standard print */
+    private static final int PRINT = 1;
+    /** Printing type for printing by order of names */
+    private static final int PRINT_NAME = 2;
+    /** Printing type for printing by order of payment date */
+    private static final int PRINT_DATE = 3;
+    /** The roster of students being managed */
     private Roster roster = new Roster();
 
+    /**
+     * The public method that runs the client
+     * Uses StringTokenizer to decode command line
+     */
     public void run(){
         System.out.println("Tuition Manager starts running.");
         Scanner scan = new Scanner(System.in);
@@ -24,6 +61,12 @@ public class TuitManager {
         }
     }
 
+    /**
+     * Checks the instruction and executes corresponding procedures
+     * Does nothing if command is invalid
+     * @param st the string tokenizer currently before command token
+     * @return returns true to continue running, or false to stop running client
+     */
     private boolean checkInstruction(StringTokenizer st){
         String command;
         try{
@@ -76,13 +119,13 @@ public class TuitManager {
             System.out.println("Calculation completed.");
         }
         else if(command.equals("P")){
-            printRoster(1);
+            printRoster(PRINT);
         }
         else if(command.equals("PN")){
-            printRoster(2);
+            printRoster(PRINT_NAME);
         }
         else if(command.equals("PT")){
-            printRoster(3);
+            printRoster(PRINT_DATE);
         }
         else{
             System.out.println("Command '" + command + "' not supported!");
@@ -90,6 +133,11 @@ public class TuitManager {
         return true;
     }
 
+    /**
+     * Checks if a profile of a student is valid
+     * @param st the string tokenizer currently before name token
+     * @return an array containing name and major, null if invalid
+     */
     private String[] validProfile(StringTokenizer st){
         String name;
         String major;
@@ -107,6 +155,11 @@ public class TuitManager {
         return new String[]{name, major};
     }
 
+    /**
+     * Checks if a major is valid
+     * @param major major to be checked
+     * @return the string of major in correct format, null if invalid
+     */
     private String validMajor(String major){
         String caseMajor = major.toUpperCase();
         if(caseMajor.equals("CS") || caseMajor.equals("IT") || caseMajor.equals("BA") ||
@@ -117,6 +170,12 @@ public class TuitManager {
         return null;
     }
 
+    /**
+     * Checks if amount of credits is valid
+     * @param st the string tokenizer currently before credits token
+     * @param isInternational tracks if student is international
+     * @return amount of credits, INVALID_CREDIT ( = 0) if invalid
+     */
     private int validCredit(StringTokenizer st, boolean isInternational){
         int credits;
         try{
@@ -147,6 +206,11 @@ public class TuitManager {
         return credits;
     }
 
+    /**
+     * Adds a Resident to roster if student does not exist already
+     * @param profile profile of student
+     * @param credits amount of credits
+     */
     private void addResident(String[] profile, int credits){
         Resident res = new Resident(profile[NAME_INDEX], profile[MAJOR_INDEX], credits);
         boolean added = roster.add(res);
@@ -159,6 +223,11 @@ public class TuitManager {
 
     }
 
+    /**
+     * Adds a NonResident to roster if student does not exist already
+     * @param profile profile of student
+     * @param credits amount of credits
+     */
     private void addNonResident(String[] profile, int credits){
         NonResident nonRes = new NonResident(profile[NAME_INDEX], profile[MAJOR_INDEX], credits);
         boolean added = roster.add(nonRes);
@@ -170,6 +239,12 @@ public class TuitManager {
         }
     }
 
+    /**
+     * Adds an International to roster if student does not exist already
+     * @param st the string tokenizer currently before abroad boolean token
+     * @param profile profile of student
+     * @param credits amount of credits
+     */
     private void addInternational(StringTokenizer st, String[] profile, int credits){
         boolean abroad = st.nextToken().equalsIgnoreCase("TRUE");
         International intern = new International(profile[NAME_INDEX], profile[MAJOR_INDEX], credits, abroad);
@@ -182,6 +257,12 @@ public class TuitManager {
         }
     }
 
+    /**
+     * Adds a TriState to roster if student does not exist already
+     * @param st the string tokenizer currently before state token
+     * @param profile profile of student
+     * @param credits amount of credits
+     */
     private void addTriState(StringTokenizer st, String[] profile, int credits){
         String state;
         try {
@@ -203,6 +284,11 @@ public class TuitManager {
         }
     }
 
+    /**
+     * Checks if a state is a valid tri-state
+     * @param state the state to be checked
+     * @return the string of tri-state in correct format, null if invalid
+     */
     private String validTriState(String state){
         state = state.toUpperCase();
         if(state.equals("NY") || state.equals("CT")){
@@ -212,6 +298,11 @@ public class TuitManager {
         return null;
     }
 
+    /**
+     * Make a payment towards tuition for a student if payment amount exists and is valid
+     * @param st the string tokenizer currently before payment amount token
+     * @param profile profile of target student
+     */
     private void payTuition(StringTokenizer st, String[] profile){
         double payment;
         try{
@@ -247,6 +338,11 @@ public class TuitManager {
         System.out.println("Payment applied.");
     }
 
+    /**
+     * Give a resident student financial aid if are full-time and never received aid yet
+     * @param st the string tokenizer currently before financial aid token
+     * @param profile profile of target student
+     */
     private void setFinancialAid(StringTokenizer st, String[] profile){
         double aid;
         try {
@@ -279,6 +375,11 @@ public class TuitManager {
         System.out.println("Tuition updated.");
     }
 
+    /**
+     * Checks if financial aid amount is valid
+     * @param financialAid financial aid amount
+     * @return true if valid, false otherwise
+     */
     private boolean isValidFinancialAid(double financialAid){
         if(financialAid <= 0 || financialAid > Resident.MAX_AID){
             System.out.println("Invalid amount.");
@@ -287,6 +388,10 @@ public class TuitManager {
         return true;
     }
 
+    /**
+     * Sets an International student to be studying abroad if they exist in roster
+     * @param profile profile of target student
+     */
     private void setAbroadStatus(String[] profile){
         Student temp = new Student(profile[NAME_INDEX], profile[MAJOR_INDEX]);
         Student found = roster.getAStudent(temp);
@@ -298,6 +403,10 @@ public class TuitManager {
         System.out.println("Tuition updated.");
     }
 
+    /**
+     * Removes a student from the roster if they exist
+     * @param profile profile of target student
+     */
     private void removeStudent(String[] profile){
         Student temp = new Student(profile[NAME_INDEX], profile[MAJOR_INDEX]);
         if(roster.remove(temp)){
@@ -308,21 +417,25 @@ public class TuitManager {
         }
     }
 
+    /**
+     * Prints the roster given the type of order
+     * @param type the type of printing wanted
+     */
     private void printRoster(int type){
         if(roster.getSize() == Roster.EMPTY){
             System.out.println("Student roster is empty!");
             return;
         }
         switch (type) {
-            case 1 -> {
+            case PRINT -> {
                 System.out.println("* list of students in the roster **");
                 roster.print();
             }
-            case 2 -> {
+            case PRINT_NAME -> {
                 System.out.println("* list of students ordered by name **");
                 roster.printByName();
             }
-            case 3 -> {
+            case PRINT_DATE -> {
                 System.out.println("* list of students made payments ordered by payment date **");
                 roster.printByPaymentDate();
             }
